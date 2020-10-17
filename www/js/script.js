696eae05a69e00e5aa353e74e022ee7bf22b6807
nikitenkovit <nelot@mail.ru>
2020-10-17 20:53:45 +0300

    upgrade menu handler

--- www/js/script.js
+++ www/js/script.js
@@ -5,22 +5,46 @@
   var shedule = document.querySelector('.shedule');
   var takeClass = document.querySelector('.takeClass');
   var body = document.querySelector('body');
+  var nav = document.querySelector('nav')
 
   /*toggle menu*/
 
-  navToggleButton.addEventListener('click', function () {
+  var openMenu = function () {
+    navWrapper.style.overflow = 'visible';
+    takeClass.style.display = 'none';
+    // nav.style.position = 'fixed';
+  };
+
+  var closeMenu = function () {
+    navWrapper.style.overflow = 'hidden';
+    takeClass.style.display = 'block';
+    nav.style.position = 'relative';
+  };
+
+  var menuHandler = function () {
     navToggleButton.classList.toggle('nav__toggle-button--open');
     menu.classList.toggle('menu--open');
     if (menu.classList.contains('menu--open')) {
-      navWrapper.style.overflow = 'visible';
-      takeClass.style.display = 'none';
+      openMenu();
+      document.addEventListener('click', missClick);
     } else {
-      navWrapper.style.overflow = 'hidden';
-      takeClass.style.display = 'block';
+      closeMenu();
+      document.removeEventListener('click', missClick);
     }
     shedule.classList.toggle('visually-hidden');
-    body.classList.toggle('darkBackground')
-  })
+    body.classList.toggle('darkBackground');
+  }
+
+  navToggleButton.addEventListener('click', menuHandler)
+
+  var missClick = function (evt) {
+    var target = evt.target;
+    var itsNav = target === nav || nav.contains(target);
+    var itsMenu = target === menu || menu.contains(target);
+    if (!itsNav && !itsMenu) {
+      menuHandler();
+    }
+  };
 
   /*change justify-content value in photoSlider from mini-groups page*/
 
