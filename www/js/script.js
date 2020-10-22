622277c697d548c0ed189cfcc2348d54479a54a7
nikitenkovit <nelot@mail.ru>
2020-10-22 23:41:58 +0300

    bags"

--- www/js/script.js
+++ www/js/script.js
@@ -80,4 +80,25 @@
   jQuery(window).resize(resizeColorBox);
   window.addEventListener("orientationchange", resizeColorBox, false);
 
+  /*up-button*/
+
+  var upBtn = document.querySelector('.up-button')
+
+  window.onscroll = function () {
+    if (window.pageYOffset > 200) {
+      upBtn.classList.remove('up-button--hidden')
+    } else {
+      upBtn.classList.add('up-button--hidden')
+    }
+  }
+
+  function backToTop() {
+    if (window.pageYOffset > 0) {
+      window.scrollBy(0, -80);
+      setTimeout(backToTop, 10);
+    }
+  }
+
+  upBtn.addEventListener('click', backToTop);
+
 })();
